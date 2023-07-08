440a5be430e0e0dbe07dcda89908a780fe03171e
wyhong3103 <wyhong3103@gmail.com>
2023-07-08 20:38:07 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const indexRouter = require('./routes/index');
+
+const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
+const mongoDB = MONGODB_URI;
+
+main.catch(err => console.log(err));
+async function main(){
+    await mongoose.connect(mongoDB);
+}
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
