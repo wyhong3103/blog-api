2aa92a5e0fac04c6f60b0c75506a1c963ee10fe2
wyhong3103 <wyhong3103@gmail.com>
2023-07-09 16:05:28 +0800

    Add all routes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const authController = require('../controllers/authController');
+const blogController = require('../controllers/blogController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//Login
+router.post('/login', authController.login);
+
+//Register
+router.post('/signup', authController.register);
+
+//Get all the blogs
+router.get('/blog', blogController.get_blogs);
+
+//Post blog
+router.post('/blog', authController.verifyToken, blogController.post_blog);
+
+//Get a blog
+router.get('/blog/:id', authController.verifyToken, blogController.get_blog);
+
+//Post comment
+router.post('/blog/:id', authController.verifyToken, blogController.post_comment);
+
+//Update blog
+router.put('/blog/:id', authController.verifyToken, blogController.update_blog);
+
+//Delete blog
+router.delete('/blog/:id', authController.verifyToken, blogController.delete_blog);
+
+//Update comment
+router.put('/blog/:blogid/comment/:id', authController.verifyToken, blogController.update_comment);
+
+//Delete comment
+router.delete('/blog/:blogid/comment/:id', authController.verifyToken, blogController.delete_comment);
+
 
 module.exports = router;
